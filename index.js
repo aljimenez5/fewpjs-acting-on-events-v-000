4d7e496638137202d2a94a859a39f98f4f1d197c
Arvida Jimenez <arvida.jimenez@gmail.com>
2019-12-26 03:43:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ document.addEventListener("keydown", function(e) {
 });
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.left.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let right = parseInt(leftNumbers, 10);
  
   if (right > 0) {
     dodger.style.right = `${right - 1}px`;
