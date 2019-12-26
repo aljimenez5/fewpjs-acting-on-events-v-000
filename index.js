e6657144cac30c52e88644b0081e45284b491ffd
Arvida Jimenez <arvida.jimenez@gmail.com>
2019-12-26 03:44:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10);
  
   if (right > 0) {
-    dodger.style.left = `${left +F 1}px`;
+    dodger.style.left = `${left + 1}px`;
   }
 }
